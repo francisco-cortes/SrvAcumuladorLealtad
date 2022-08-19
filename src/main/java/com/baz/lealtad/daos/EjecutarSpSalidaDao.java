42805cd8ab71f6f01d53ffd2c35fe4e1a980bcd4
Francisco Javier Cortes Torres <fj23pk@hotmail.com>
2022-08-19 13:03:03 -0500

    cambios a la clase ejecutarspSalidaDao basados en sonarqube

--- src/main/java/com/baz/lealtad/daos/EjecutarSpSalidaDao.java
+++ src/main/java/com/baz/lealtad/daos/EjecutarSpSalidaDao.java
@@ -15,29 +15,38 @@ import java.util.List;
 public class EjecutarSpSalidaDao {
 
 
-    private static final Logger log = Logger.getLogger(EjecutarSpSalidaDao.class);
-    private final FabricaDaoUtil fabricaDao = new FabricaDaoUtil();
+    private static final Logger LOGGER = Logger.getLogger(EjecutarSpSalidaDao.class);
+    private static final FabricaDaoUtil fabricaDao = new FabricaDaoUtil();
 
 
     public List<CursorSpSalidaModel> ejecutarSpSalida(){
+
         List<CursorSpSalidaModel> listaCursor = new ArrayList<>();
+
+        final int cursorRespuesta = 1, mensajeRespuesta = 2, codigoRespuesta = 3;
 
         Connection conexion = null;
         CallableStatement declaracion = null;
         ResultSet resultSet = null;
 
         try {
+
             conexion = fabricaDao.obtenerConexion();
+
             declaracion = conexion.prepareCall(ParametrerConfiguration.ORACLE_DATABASE_STOREPROCEDURE);
-            declaracion.registerOutParameter(1, OracleTypes.REF_CURSOR);
-            declaracion.registerOutParameter(2, OracleTypes.VARCHAR);
-            declaracion.registerOutParameter(3, OracleTypes.VARCHAR);
+            declaracion.registerOutParameter(cursorRespuesta, OracleTypes.REF_CURSOR);
+            declaracion.registerOutParameter(mensajeRespuesta, OracleTypes.VARCHAR);
+            declaracion.registerOutParameter(codigoRespuesta, OracleTypes.VARCHAR);
             declaracion.executeQuery();
 
             resultSet = (ResultSet) declaracion.getObject(1);
+
             if(resultSet != null){
+
                 while (resultSet.next()){
+
                     CursorSpSalidaModel cursor = new CursorSpSalidaModel();
+
                     cursor.setFNIDTIPOCLIENTE(resultSet.getInt("FNIDTIPOCLIENTE"));
                     cursor.setFCIDCLIENTE(resultSet.getString("FCIDCLIENTE"));
                     cursor.setFNIMPORTE(resultSet.getInt("FNIMPORTE"));
@@ -51,24 +60,33 @@ public class EjecutarSpSalidaDao {
                     cursor.setFIPAISID(resultSet.getInt("FIPAISID"));
                     listaCursor.add(cursor);
                 }
+
             }
             else {
-                log.error("SPPUNTOSLEALTAD no ejecutado o respuesta nula");
-            }
 
+                LOGGER.error("SPPUNTOSLEALTAD no ejecutado o respuesta nula");
+
+            }
         }
         catch (Exception excepcion){
-            log.error("Error en db: \n" + excepcion);
+
+            LOGGER.error("Error en db: \n" + excepcion);
+
         }
         finally {
+
             try {
                 fabricaDao.cerrarConexion(conexion, declaracion, resultSet);
             }
             catch (Exception e) {
-                log.error("Error al cerrar conexion : \n" + e);
+
+                LOGGER.error("Error al cerrar conexion : \n" + e);
             }
+
         }
+
         return listaCursor;
+
     }
 
 }
