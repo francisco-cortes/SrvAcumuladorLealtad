5c1b629ab84457a1a621b7f8e02d4a33e76e154c
Francisco Javier Cortes Torres <fj23pk@hotmail.com>
2022-08-26 17:11:51 -0500

    bloque duplicado elminiado en tokenDao

--- src/main/java/com/baz/lealtad/daos/TokenDao.java
+++ src/main/java/com/baz/lealtad/daos/TokenDao.java
@@ -2,19 +2,14 @@ package com.baz.lealtad.daos;
 
 import com.baz.lealtad.configuration.ParametrerConfiguration;
 import com.baz.lealtad.utils.ConectorHttpsUtil;
-import com.baz.lealtad.utils.GetCertUtil;
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
-
-import javax.net.ssl.TrustManagerFactory;
-import javax.net.ssl.SSLContext;
 import javax.net.ssl.HttpsURLConnection;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.UnsupportedEncodingException;
-import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
@@ -80,7 +75,33 @@ public class TokenDao {
         wr.writeBytes(form);
         wr.close();
 
+        BufferedReader br;
         if(connection.getResponseCode() > ParametrerConfiguration.OK_STATUS_CODE_LIMIT){
+            br = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+        }
+        else {
+            br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+        }
+
+        br.close();
+
+        if (connection.getResponseCode() > ParametrerConfiguration.OK_STATUS_CODE_LIMIT){
+            LOGGER.error(connection.getResponseCode() + " Error en Token: " + sb);
+        }
+        else {
+            JSONObject jsonResponse = new JSONObject(sb.toString());
+            token = jsonResponse.getString("access_token");
+            LOGGER.info("Token: " + token);
+        }
+        connection.disconnect();
+
+        /*if(connection.getResponseCode() > ParametrerConfiguration.OK_STATUS_CODE_LIMIT){
 
             BufferedReader errorReader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
             StringBuilder errorResponse = new StringBuilder();
@@ -111,7 +132,7 @@ public class TokenDao {
             JSONObject jsonResponse = new JSONObject(sb.toString());
             token = jsonResponse.getString("access_token");
             LOGGER.info("Token: " + token);
-        }
+        }*/
 
         return token;
 
